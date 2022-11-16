255c14217d90d0926e0fedda09c57792d3456b3a
kennedybabu <kennedynjogu8@gmail.com>
2022-11-16 13:01:46 +0300

    feature: style the landing page

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import  Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import {BsArrowRightCircle} from 'react-icons/bs'
 const Landingpage = () => {
   return (
-    <div>
-        
-    </div>
+    <Container className='landing'>
+        <h2>quikk money sending...</h2>
+        <div className="overlay"></div>       
+        <Button className='btn btn--landing'>Get started <BsArrowRightCircle /></Button>{' '} 
+    </Container>
   )
 }
 
