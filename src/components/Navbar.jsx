d3539645766f97a3075de1e0855e084c52a865e7
kennedybabu <kennedynjogu8@gmail.com>
2022-11-16 12:26:03 +0300

    feature: create the navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Container  from 'react-bootstrap/Container';
+import Navbar  from 'react-bootstrap/Navbar';
+
+
+const Nav = () => {
+  return (
+    <Navbar className='nav'>
+      <Container className='nav-container'>
+          <Navbar.Brand href="#home">quikk</Navbar.Brand>          
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>Home</Navbar.Text>
+          </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Nav
